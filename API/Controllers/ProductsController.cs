1d0ebc05894ae3b76240955133b4a367fdf021ce
SHRENIKYD <Shrenikcsd@gmail.com>
2024-11-26 10:29:36 +0530

    End of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,26 +1,19 @@
-using System;
+using API.RequestHelpers;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specification;
-using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-
-public class ProductsController(IGenericRepository<Product> repository) : ControllerBase
+public class ProductsController(IGenericRepository<Product> repository) : BaseApiController
 {
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Product>>> GetProduct(string? brand, string? type, 
-        string? sort)
+    public async Task<ActionResult<IEnumerable<Product>>> GetProduct([FromQuery]ProductSpecPrams specParams)
     {
-
-        var spec = new ProductSpecification(brand, type,sort);
-        var products = await repository.ListAsync(spec);
-        return Ok(products);
+        var spec = new ProductSpecification(specParams);
+        
+        return await CreatePagedResult(repository, spec, specParams.PageIndex, specParams.PageSize);
     }
 
     [HttpGet("{id:int}")]
--- Infrastructure/Data/SpectficationEvaluator.cs
+++ Infrastructure/Data/SpectficationEvaluator.cs
@@ -23,6 +23,10 @@ public class SpectficationEvaluator<T> where T : BaseEntity
             query = query.Distinct();
         }
 
+        if(spec.IsPagingEnabled){
+            query = query.Skip(spec.Skip).Take(spec.Take);
+        }
+
         return query;
     }
     public static IQueryable<TResult> GetQuery<TSpec, TResult>(IQueryable<T> query, ISpecification<T, TResult> spec)
@@ -47,6 +51,9 @@ public class SpectficationEvaluator<T> where T : BaseEntity
 
         if(spec.IsDistinct){
             selectQuery = selectQuery?.Distinct();
+        }
+        if(spec.IsPagingEnabled){
+            selectQuery = selectQuery?.Skip(spec.Skip).Take(spec.Take);
         }
         
         return selectQuery ?? query.Cast<TResult>();
